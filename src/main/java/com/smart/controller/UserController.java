9c463f6925ad5fd61025b2b53ded7335ffcfdb65
rahul_marathe <maratherahul6595@gmail.com>
2021-06-04 17:11:51 +0530

    Code changes for add contact

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -5,9 +5,13 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -17,9 +21,9 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@RequestMapping("/index")
-	public String dashboard(Model model, Principal principal) {
-		
+	//method for adding common data to response
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal) {
 		String username=principal.getName();
 		System.out.println("Username :: "+username);
 		
@@ -29,9 +33,36 @@ public class UserController {
 		model.addAttribute(user);
 		
 		System.out.println("Username: 'User' is default in spring boot..");
-		
-		return "normal/user_dashboard";
-		
 	}
 	
+	@RequestMapping("/index")
+	public String dashboard(Model model, Principal principal) {
+		model.addAttribute("title","User Dashboard");
+		return "normal/user_dashboard";
+	}
+
+	//open add form handler
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) {
+		model.addAttribute("title","Add Contact");
+		model.addAttribute("contact",new Contact());
+		
+		return "normal/add_contact_form";
+	}
+	
+	//processing add contact
+	@PostMapping("/process-contact")
+	public String processContact(@ModelAttribute Contact contact, Principal principal) {
+		
+		String name=principal.getName();
+		User user=userRepository.getUserByUserName(name);
+		
+		contact.setUser(user);
+		user.getContacts().add(contact);
+		this.userRepository.save(user);
+		System.out.println("User :: "+user);
+		System.out.println("Data ::"+contact);
+		System.out.println("User Details Added Successfully..");
+		return "normal/add_contact_form";
+	}
 }
--- src/main/java/com/smart/entities/Contact.java
+++ src/main/java/com/smart/entities/Contact.java
@@ -94,5 +94,13 @@ public class Contact {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 	
 }
